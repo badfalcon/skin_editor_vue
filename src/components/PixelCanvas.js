8ba4312e56b238f84c1dfaff60ee2e9f25d65469
badfalcon <badfalcon610@gmail.com>
2021-10-28 02:21:14 +0900

    let layer drawable zoomed

---
+++ src/components/PixelCanvas.js
@@ -0,0 +1,165 @@
+export default class PixelCanvas{
+  dom = null;
+  ctx = null;
+  hiddenCanvas = null;
+  prev_pt = null;
+  toolIndex = 0;
+  width;
+  height;
+  pixelWidth;
+  pixelHeight;
+
+  constructor(width = 400,height = 400,pixelWidth = 64,pixelHeight = 64) {
+    this.width = width;
+    this.height = height;
+    this.pixelWidth = pixelWidth;
+    this.pixelHeight = pixelHeight;
+    this.initialize();
+  }
+
+  initialize(){
+    this.dom = document.createElement("canvas");
+    this.dom.getContext('2d').imageSmoothingEnabled = false;
+    this.dom.width = this.width;
+    this.dom.height = this.height;
+    this.hiddenCanvas = document.createElement("canvas");
+    this.hiddenCanvas.width = this.pixelWidth;
+    this.hiddenCanvas.height = this.pixelHeight;
+    this.setContext(this.hiddenCanvas.getContext('2d'));
+  }
+
+  setContext(ctx) {
+    this.ctx = ctx;
+    this.imageData = this.ctx.getImageData(0, 0, this.pixelWidth, this.pixelHeight)
+    this.repaint()
+  }
+
+  setTool(index){
+    this.toolIndex = index;
+  }
+
+  repaint() {
+    this.ctx.putImageData(this.imageData, 0, 0)
+    let ctx = this.dom.getContext('2d')
+    ctx.drawImage(this.hiddenCanvas, 0, 0, this.width, this.height)
+  }
+
+  onPointerOver(e) {
+    // eslint-disable-next-line no-unused-vars
+    let pt = this.getPoint(e)
+    return false;
+  }
+
+  onPointerEnter(e) {
+    // eslint-disable-next-line no-unused-vars
+    let pt = this.getPoint(e)
+    return false;
+  }
+  onPointerDown(e) {
+    let pt = this.getPoint(e)
+    switch (this.toolIndex){
+      case 0:
+        if(pt){
+          this.setDot(pt.x, pt.y, [255, 0, 0, 255])
+          this.prev_pt = pt
+          this.repaint()
+          return true;
+        }
+        break;
+    }
+    return false;
+  }
+  onPointerMove(e) {
+    //todo pressure使うと出て戻ってきたときとかにややこしくなるからフラグで管理でも
+    let pt = this.getPoint(e)
+    switch (this.toolIndex) {
+      case 0:
+        if(e.pressure && pt){
+          this.setLine(this.prev_pt.x, this.prev_pt.y, pt.x, pt.y, [255, 0, 0, 255])
+          this.prev_pt = pt
+          this.repaint()
+          return true;
+        }
+        break;
+    }
+    return false;
+  }
+  onPointerUp(e) {
+    // eslint-disable-next-line no-unused-vars
+    let pt = this.getPoint(e)
+    return false;
+  }
+  onPointerOut(e) {
+    // eslint-disable-next-line no-unused-vars
+    let pt = this.getPoint(e)
+    return false;
+  }
+  onPointerLeave(e) {
+    // eslint-disable-next-line no-unused-vars
+    let pt = this.getPoint(e)
+    return false;
+  }
+
+  getPoint(e) {
+    let clientRect = e.target.getBoundingClientRect();
+    let rawPt = {
+      x: (e.clientX - clientRect.left) / this.width * this.pixelWidth,
+      y: (e.clientY - clientRect.top) / this.height * this.pixelHeight
+    };
+    return (0 <= rawPt.x && rawPt.x <= this.width && 0 <= rawPt.y && rawPt.y <= this.height) ? {
+      x: Math.floor(rawPt.x),
+      y: Math.floor(rawPt.y)
+    } : false;
+  }
+
+  setDot(x, y, color) {
+    let data = this.imageData.data
+    let index = (this.imageData.width * y + x) * 4
+    data[index] = color[0]
+    data[index + 1] = color[1]
+    data[index + 2] = color[2]
+    data[index + 3] = color[3]
+  }
+
+  getDot(x,y){
+    let data = this.imageData.data
+    let index = (this.imageData.width * y + x) * 4
+    return data.slice(index,index + 3);
+  }
+
+  setLine(x1,y1,x2,y2,color){
+    let steep = Math.abs(y2 - y1) > Math.abs(x2 - x1)
+    if(steep){
+      let tmp = x1;
+      x1 = y1;
+      y1 = tmp;
+
+      tmp = x2;
+      x2 = y2;
+      y2 = tmp;
+    }
+    if(x1 > x2){
+      let tmp = x1;
+      x1 = x2;
+      x2 = tmp;
+
+      tmp = y1;
+      y1 = y2;
+      y2 = tmp;
+    }
+    let dx = x2 - x1
+    let dy = Math.abs(y2 - y1)
+    let error = dx / 2
+    let y = y1
+    let y_step = y1 < y2 ? 1: -1
+    for(let x = x1; x <= x2; x++){
+      steep ? this.setDot(y,x,color) : this.setDot(x,y,color)
+      error -= dy
+      if(error < 0) {
+        y += y_step
+        error += dx
+      }
+    }
+  }
+
+}
--- src/components/SkinEditor.vue
+++ src/components/SkinEditor.vue
@@ -7,6 +7,7 @@
       <button v-for="(value,index) in tools" :key="'tool'+index" @click="selectTool(index)">{{value.name}}</button>
       {{currentTool}},{{tools}}
     </div>
+<!--    <PixelCanvas ref="mainCanvas" :width=400 :height=400 :pixel-width=64 :pixel-height=64></PixelCanvas>-->
     <canvas id="mainCanvas" ref="mainCanvas" width="400" height="400"
             v-on="{pointerover:onPointerOver,pointerenter:onPointerEnter,pointerdown:onPointerDown,pointermove:onPointerMove,pointerup:onPointerUp,pointerout:onPointerOut,pointerleave:onPointerLeave}"></canvas>
     <LayerManager ref="layerManager" @repaint="repaint" @updateLayer="updateLayer"></LayerManager>
@@ -17,6 +18,7 @@
 import LayerManager from "@/components/LayerManager";
 import Pen from "@/components/Pen";
 import Eraser from "@/components/Eraser";
+import PixelCanvas from "@/components/PixelCanvas";
 
 export default {
   name: "SkinEditor",
@@ -27,76 +29,90 @@ export default {
       currentLayer: null,
 
       tools: [new Pen,new Eraser],
+      pixelCanvas: new PixelCanvas(),
       ctx: null,
       currentTool:-1
     }
   },
   methods: {
     repaint() {
-      console.log("[Skin Editor] repaint")
       let ctx = this.$refs.mainCanvas.getContext('2d')
       ctx.clearRect(0, 0, 400, 400)
       this.$refs.layerManager.layers.forEach((layer) => {
         if (layer.visible) {
           ctx.drawImage(layer.dom, 0, 0,400,400)
-
         }
       })
     },
 
     selectTool(index){
       this.currentTool = index
+      this.pixelCanvas.setTool(index)
     },
 
     onPointerEnter(e) {
-      if(typeof this.tools[this.currentTool].onPointerEnter == "function"){
-        let pt = this.getPoint(e)
-        let repaintFlg = this.tools[this.currentTool].onPointerEnter(this.ctx,pt)
-        if(repaintFlg) this.repaint()
-      }
+      let repaintFlg = this.pixelCanvas.onPointerEnter(e)
+      if(repaintFlg) this.repaint()
+      // if(typeof this.tools[this.currentTool].onPointerEnter == "function"){
+      //   let pt = this.getPoint(e)
+      //   let repaintFlg = this.tools[this.currentTool].onPointerEnter(this.ctx,pt)
+      //   if(repaintFlg) this.repaint()
+      // }
     },
     onPointerOver(e) {
-      if(typeof this.tools[this.currentTool].onPointerOver == "function"){
-        let pt = this.getPoint(e)
-        let repaintFlg =this.tools[this.currentTool].onPointerOver(this.ctx,pt)
-        if(repaintFlg) this.repaint()
-      }
+      let repaintFlg =this.pixelCanvas.onPointerOver(e)
+      if(repaintFlg) this.repaint()
+      // if(typeof this.tools[this.currentTool].onPointerOver == "function"){
+      //   let pt = this.getPoint(e)
+      //   let repaintFlg =this.tools[this.currentTool].onPointerOver(this.ctx,pt)
+      //   if(repaintFlg) this.repaint()
+      // }
     },
     onPointerDown(e) {
-      if(typeof this.tools[this.currentTool].onPointerDown == "function"){
-        let pt = this.getPoint(e)
-        console.log(pt)
-        let repaintFlg =this.tools[this.currentTool].onPointerDown(this.ctx,pt)
-        if(repaintFlg) this.repaint()
-      }
+      let repaintFlg = this.pixelCanvas.onPointerDown(e)
+      if(repaintFlg) this.repaint()
+      // if(typeof this.tools[this.currentTool].onPointerDown == "function"){
+      //   let pt = this.getPoint(e)
+      //   console.log(pt)
+      //   let repaintFlg =this.tools[this.currentTool].onPointerDown(this.ctx,pt)
+      //   if(repaintFlg) this.repaint()
+      // }
     },
     onPointerMove(e) {
-      if(typeof this.tools[this.currentTool].onPointerMove == "function"){
-        let pt = this.getPoint(e)
-        let repaintFlg =this.tools[this.currentTool].onPointerMove(this.ctx,pt)
-        if(repaintFlg) this.repaint()
-      }
+      let repaintFlg = this.pixelCanvas.onPointerMove(e)
+      if(repaintFlg) this.repaint()
+      // if(typeof this.tools[this.currentTool].onPointerMove == "function"){
+      //   let pt = this.getPoint(e)
+      //   let repaintFlg =this.tools[this.currentTool].onPointerMove(this.ctx,pt)
+      //   if(repaintFlg) this.repaint()
+      // }
     },
     onPointerUp(e) {
-      if(typeof this.tools[this.currentTool].onPointerUp == "function"){
-        let pt = this.getPoint(e)
-        let repaintFlg =this.tools[this.currentTool].onPointerUp(this.ctx,pt)
-        if(repaintFlg) this.repaint()
-      }
+      let repaintFlg = this.pixelCanvas.onPointerUp(e)
+      if(repaintFlg) this.repaint()
+      // if(typeof this.tools[this.currentTool].onPointerUp == "function"){
+      //   let pt = this.getPoint(e)
+      //   let repaintFlg =this.tools[this.currentTool].onPointerUp(this.ctx,pt)
+      //   if(repaintFlg) this.repaint()
+      // }
     },
     onPointerOut(e) {
-      if(typeof this.tools[this.currentTool].onPointerOut == "function"){
-        let pt = this.getPoint(e)
-        let repaintFlg =this.tools[this.currentTool].onPointerOut(this.ctx,pt)
-        if(repaintFlg) this.repaint()
-      }
+      let repaintFlg = this.pixelCanvas.onPointerOut(e)
+      if(repaintFlg) this.repaint()
+      // if(typeof this.tools[this.currentTool].onPointerOut == "function"){
+      //   let pt = this.getPoint(e)
+      //   let repaintFlg =this.tools[this.currentTool].onPointerOut(this.ctx,pt)
+      //   if(repaintFlg) this.repaint()
+      // }
     },
     onPointerLeave(e) {
-      if(typeof this.tools[this.currentTool].onPointerLeave == "function"){
-        let pt = this.getPoint(e)
-        let repaintFlg =this.tools[this.currentTool].onPointerLeave(this.ctx,pt)
-        if(repaintFlg) this.repaint()
-      }
+      let repaintFlg = this.pixelCanvas.onPointerLeave(e)
+      if(repaintFlg) this.repaint()
+      // if(typeof this.tools[this.currentTool].onPointerLeave == "function"){
+      //   let pt = this.getPoint(e)
+      //   let repaintFlg =this.tools[this.currentTool].onPointerLeave(this.ctx,pt)
+      //   if(repaintFlg) this.repaint()
+      // }
     },
     getPoint(e) {
       let clientRect = e.target.getBoundingClientRect();
@@ -106,6 +122,7 @@ export default {
     updateLayer(){
       this.currentLayer = this.$refs.layerManager.getLayer()
       this.ctx = this.currentLayer.dom.getContext('2d')
+      this.pixelCanvas.setContext(this.ctx)
     }
 
   },
@@ -114,8 +131,8 @@ export default {
   },
   mounted() {
     console.log("[Skin Editor] mounted")
-    // this.currentLayer = this.$refs.layerManager.getLayer()
-    // this.ctx = this.currentLayer.dom.getContext('2d')
+    this.currentLayer = this.$refs.layerManager.getLayer()
+    this.ctx = this.currentLayer.dom.getContext('2d')
     let ctx = this.$refs.mainCanvas.getContext('2d')
     ctx.imageSmoothingEnabled = false;
     this.selectTool(0)
