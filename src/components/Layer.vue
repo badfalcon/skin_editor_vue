6b8a67938237a2c0e755d0ec7718826e67aff895
badfalcon <badfalcon610@gmail.com>
2021-11-13 03:27:18 +0900

    [add] rename function

--- src/components/Layer.vue
+++ src/components/Layer.vue
@@ -1,9 +1,13 @@
 <template>
-  <div  draggable="true" :id="'layer_' + layer_obj.id" ref="layer" @click="select"
+  <div :draggable="!isChangingName" :id="'layer_' + layer_obj.id" ref="layer" @click="select"
        :class="[{hidden: !layer_obj.visible},'layer']"
        @click.right.prevent="openLayerMenu">
-<!--    <canvas class="layer-thumbnail" ref="layerCanvas" width="50px" height="50px"></canvas>-->
-    <span class="layer_name">{{ layer_obj.nickname }}</span> <!--todo check-->
+    <!--    <canvas class="layer-thumbnail" ref="layerCanvas" width="50px" height="50px"></canvas>-->
+    <span class="layer_name" @click.stop @dblclick="renameStart"
+          v-show="!isChangingName">{{ layer_obj.nickname }}</span> <!--todo check-->
+    <input class="layer_rename" ref="renameInput" @click.stop @dblclick.stop type="text"
+           :value="layer_obj.nickname" @blur="renameEnd" @keydown.enter="renameEnd" maxlength="20"
+           v-show="isChangingName">
     <button class="layer_visible" @click="toggle">sh/hi</button>
     <button class="layer_delete" @click="remove" :disabled="sole_layer">rem</button>
     <button class="layer_up" @click="up" :disabled="sole_layer">↑</button>
@@ -13,13 +17,28 @@
 
 <script>
 export default {
-  props: ['layer_obj','sole_layer'],
+  props: ['layer_obj', 'sole_layer'],
   name: "Layer",
   data() {
     return {
+      isChangingName: false
     }
   },
   methods: {
+    renameStart(e) {
+      console.log("rename start");
+      this.isChangingName = true
+      this.$nextTick(() => this.$refs.renameInput.focus())
+      // this.$emit("layer-rename")
+      e.stopPropagation();
+    },
+    renameEnd(e) {
+      console.log("rename end");
+      console.log(e);
+      this.isChangingName = false
+      this.layer_obj.nickname = e.target.value
+      e.stopPropagation();
+    },
     select() {
       console.log("select");
       this.$emit("layer-select")
@@ -28,7 +47,7 @@ export default {
       this.$emit("layer-toggle")
       e.stopPropagation();
     },
-    remove(e){
+    remove(e) {
       this.$emit("layer-remove")
       e.stopPropagation();
     },
@@ -44,13 +63,19 @@ export default {
       console.log("openLayerMenu")
 
       // todo context menu
+    },
+    openModal() {
+      this.showContent = true
+    },
+    closeModal() {
+      this.showContent = false
     }
   },
   created() {
 
   },
   mounted() {
-    this.$refs.layer.insertBefore(this.layer_obj.dom,this.$refs.layer.firstChild)
+    this.$refs.layer.insertBefore(this.layer_obj.dom, this.$refs.layer.firstChild)
     // let ctx = this.$refs.layerCanvas.getContext('2d')
     // ctx.drawImage(this.layer_obj.dom, 0, 0)
   }
@@ -62,6 +87,7 @@ div.currentLayer {
   border: solid 1px;
   background: lightgray;
 }
+
 .layer {
   display: flex;
   justify-content: center;
