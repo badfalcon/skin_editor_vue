4a4f29d0b6c2c4408b685bb2474248a5dfad3f27
badfalcon <badfalcon610@gmail.com>
2021-11-16 01:31:14 +0900

    [merge] layer manager to SkinEditor body

--- src/components/SkinEditor.vue
+++ src/components/SkinEditor.vue
@@ -16,20 +16,29 @@
         <canvas id="mainCanvas" ref="mainCanvas" width="400" height="400"
                 v-on="{pointerover:onPointerOver,pointerenter:onPointerEnter,pointerdown:onPointerDown,pointermove:onPointerMove,pointerup:onPointerUp,pointerout:onPointerOut,pointerleave:onPointerLeave}"></canvas>
       </div>
-      <LayerManager id="layerManager" ref="layerManager" @repaint="repaint" @updateLayer="updateLayer"></LayerManager>
+      <div id="layerManager">
+        <!--    <div class="debug">{{ name }},{{ current }},{{ nextIndex }},{{ layers }}</div>-->
+        <button @click="addLayer">add layer</button>
+        <!--    <button @click="repaint">親のイベントを呼び出す</button>-->
+        <Layer v-for="layer in reversedLayers" v-bind:key="layer.id" :layer_obj="layer" :sole_layer="isSoleLayer"
+               @layer-up="up(layer)" @layer-down="down(layer)"
+               @layer-toggle="toggle(layer)" @layer-select="select(layer)" @layer-remove="remove(layer)"
+               :class="{currentLayer: layer.id===current}"></Layer>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import LayerManager from "@/components/LayerManager";
 import Pen from "@/components/Pen";
 import Eraser from "@/components/Eraser";
 import PixelCanvas from "@/components/PixelCanvas";
+import layerObject from "@/components/LayerObject";
+import Layer from "@/components/Layer";
 
 export default {
   name: "SkinEditor",
-  components: {LayerManager},
+  components: {Layer},
   data() {
     return {
       name: 'SkinEditor',
@@ -38,14 +47,27 @@ export default {
       tools: [new Pen,new Eraser],
       pixelCanvas: new PixelCanvas(),
       ctx: null,
-      currentTool:-1
+      currentTool:-1,
+
+      layers: [],
+      current: 0,
+      nextIndex: 0
+    }
+  },
+  computed: {
+    reversedLayers:function(){
+      return this.layers.slice().reverse();
+    },
+    isSoleLayer:function(){
+      console.log("sole layer is "+(this.layers.length === 1)?"true":"false")
+      return this.layers.length === 1;
     }
   },
   methods: {
     repaint() {
       let ctx = this.$refs.mainCanvas.getContext('2d')
       ctx.clearRect(0, 0, 400, 400)
-      this.$refs.layerManager.layers.forEach((layer) => {
+      this.layers.forEach((layer) => {
         if (layer.visible) {
           ctx.drawImage(layer.dom, 0, 0,400,400)
         }
@@ -136,10 +158,66 @@ export default {
     },
 
     updateLayer(){
-      this.currentLayer = this.$refs.layerManager.getLayer()
+      this.currentLayer = this.getLayer()
       this.ctx = this.currentLayer.dom.getContext('2d')
       this.pixelCanvas.setContext(this.ctx)
+    },
+
+    select(layer) {
+      console.log("select " + layer.id)
+      this.current = layer.id
+      this.updateLayer()
+    },
+    toggle(layer) {
+      console.log("toggle " + layer.id)
+      layer.toggleVisible()
+      this.repaint()
+    },
+    up(layer) {
+      console.log("up " + layer.id)
+      let sourceIndex = this.layers.findIndex(value => value.id === layer.id)
+      if(sourceIndex === this.layers.length - 1) return;
+      let targetIndex  = sourceIndex + 1
+      this.layers.splice(sourceIndex, 2, this.layers[targetIndex], this.layers[sourceIndex])
+      this.repaint()
+    },
+    down(layer) {
+      console.log("down " + layer.id)
+      let sourceIndex = this.layers.findIndex(value => value.id === layer.id)
+      if(sourceIndex === 0) return;
+      let targetIndex  = sourceIndex - 1
+      this.layers.splice(targetIndex, 2, this.layers[sourceIndex], this.layers[targetIndex])
+      this.repaint()
+    },
+    remove(layer) {
+      console.log("remove " + layer.id)
+      let layerId = layer.id;
+      let targetIndex = this.layers.findIndex(value => value.id === layerId)
+      this.layers.splice(targetIndex,1)
+      if(targetIndex === this.layers.length){
+        // last index
+        this.select(this.layers[targetIndex-1])
+      }else{
+        if(this.current === layerId){
+          this.select(this.layers[targetIndex])
+        }
+      }
+      this.repaint()
+    },
+    addLayer() {
+      console.log("addLayer")
+      let newLayer = new layerObject(this.nextIndex)
+      let currentIndex = this.layers.findIndex(value => value.id === this.current)
+      console.log("currentIndex = " + currentIndex)
+      this.layers.splice(currentIndex + 1, 0, newLayer)
+      this.nextIndex += 1
+      this.select(newLayer)
+      this.repaint()
+    },
+    getLayer(n = this.current) {
+      return this.layers.find(value => value.id === n)
     }
+
 
   },
   created() {
@@ -147,7 +225,8 @@ export default {
   },
   mounted() {
     console.log("[Skin Editor] mounted")
-    this.currentLayer = this.$refs.layerManager.getLayer()
+    this.addLayer()
+    this.currentLayer = this.getLayer()
     this.ctx = this.currentLayer.dom.getContext('2d')
     let ctx = this.$refs.mainCanvas.getContext('2d')
     ctx.imageSmoothingEnabled = false;
